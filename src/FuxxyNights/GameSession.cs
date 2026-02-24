c323df5ce4b4583104a8cbe312939e27ab161a28
ehsan_hrz <ehsanheidarzadeh959@gmail.com>
2026-02-24 14:17:37 +0330

    feat: implement game session and cli loop

--- src/FuxxyNights/GameSession.cs
+++ src/FuxxyNights/GameSession.cs
@@ -13,11 +13,31 @@ public sealed class GameSession
 
     public GameSession(Func<NightType> nextNight)
     {
+        ArgumentNullException.ThrowIfNull(nextNight);
         _nextNight = nextNight;
     }
 
     public GameSummary Play(Func<int, PlayerOption> getPlayerOption, Action<string> writeLine)
     {
-        throw new NotImplementedException();
+        ArgumentNullException.ThrowIfNull(getPlayerOption);
+        ArgumentNullException.ThrowIfNull(writeLine);
+
+        var rounds = new List<RoundResult>(DefaultRoundCount);
+
+        for (var roundIndex = 0; roundIndex < DefaultRoundCount; roundIndex++)
+        {
+            var roundNumber = roundIndex + 1;
+            var option = getPlayerOption(roundNumber);
+            var night = _nextNight();
+            var round = RoundScoring.Evaluate(option, night);
+
+            rounds.Add(round);
+            writeLine($"Round {roundNumber}: night was {night}, you picked {option}, score +{round.Points}");
+        }
+
+        var finalScore = RoundScoring.CalculateTotalScore(rounds);
+        writeLine($"Final score: {finalScore}");
+
+        return new GameSummary(DefaultRoundCount, finalScore, rounds);
     }
 }
--- src/FuxxyNights/Program.cs
+++ src/FuxxyNights/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using FuxxyNights;
+
+var random = new Random();
+var session = new GameSession(() => NextNight(random));
+
+Console.WriteLine("Welcome to FuxxyNights!");
+Console.WriteLine("Survive 10 nights by choosing the right item.");
+Console.WriteLine();
+
+session.Play(PromptForOption, Console.WriteLine);
+
+static PlayerOption PromptForOption(int roundNumber)
+{
+    while (true)
+    {
+        Console.WriteLine();
+        Console.WriteLine($"Round {roundNumber}/{GameSession.DefaultRoundCount}");
+        Console.WriteLine("Choose your defense:");
+        Console.WriteLine("1) Umbrella -> beats rainy night");
+        Console.WriteLine("2) Water pump -> beats fire night");
+        Console.WriteLine("3) Tent -> beats windy night");
+        Console.WriteLine("4) Blanket -> beats cold night");
+        Console.Write("Your choice (1-4): ");
+
+        var input = Console.ReadLine();
+
+        switch (input)
+        {
+            case "1":
+                return PlayerOption.Umbrella;
+            case "2":
+                return PlayerOption.WaterPump;
+            case "3":
+                return PlayerOption.Tent;
+            case "4":
+                return PlayerOption.Blanket;
+            default:
+                Console.WriteLine("Invalid choice. Enter a number from 1 to 4.");
+                break;
+        }
+    }
+}
+
+static NightType NextNight(Random random)
+{
+    var nights = Enum.GetValues<NightType>();
+    return nights[random.Next(nights.Length)];
+}
